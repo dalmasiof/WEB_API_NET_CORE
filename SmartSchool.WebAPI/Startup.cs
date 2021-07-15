d3d4c369235a92a58518da9f2302b9f261d2be6b
Dalmasiom <Dalmasiof@gmail.com>
2021-07-15 18:34:25 -0300

    Adicionando Contexto

--- SmartSchool.WebAPI/Startup.cs
+++ SmartSchool.WebAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SmartSchool.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace SmartSchool.WebAPI
 {
@@ -25,6 +28,9 @@ namespace SmartSchool.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SmartContext>(
+                context=>context.UseSqlite(Configuration.GetConnectionString("Default"))
+            );
             services.AddControllers();
         }
 
